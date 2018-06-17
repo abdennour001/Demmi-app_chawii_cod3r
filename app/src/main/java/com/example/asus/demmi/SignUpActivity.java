aef1c056844a6c937195d32b3fb7afcf323741c2
youpes <dm_benchadi@esi.dz>
2018-06-17 21:35:28 +0100

    SignUp added

--- app/src/main/java/com/example/asus/demmi/SignUpActivity.java
+++ app/src/main/java/com/example/asus/demmi/SignUpActivity.java
@@ -4,12 +4,14 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
@@ -23,6 +25,12 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import java.util.ArrayList;
 
 /**
@@ -30,6 +38,10 @@ import java.util.ArrayList;
  */
 
 public class SignUpActivity extends AppCompatActivity {
+
+    // The Authentication reference.
+
+    private FirebaseAuth mAuth;
 
     // UI References.
 
@@ -79,6 +91,10 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        // Initializing the Authentication reference
+
+        mAuth = FirebaseAuth.getInstance();
 
         /**
          * Add all (findViews) here. **/
@@ -233,6 +249,27 @@ public class SignUpActivity extends AppCompatActivity {
         String bloodCategory = mBloodCategory.getSelectedItem().toString();
         String bloodRH = mBloodRisus.getSelectedItem().toString();
 
+        // Create a new firebase user
+        mAuth.createUserWithEmailAndPassword(email, passWord)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
 
     }
 
